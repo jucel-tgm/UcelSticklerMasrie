9df6c4e2c0f90b42813bfaf9dbe7e970105eff27
Matthias Stickler <mstickler@student.tgm.ac.at>
2014-12-05 11:52:40 +0100

    formel gefixed +pkg

--- teamarbeit/Raum.java
+++ teamarbeit/Raum.java
@@ -2,6 +2,7 @@ package teamarbeit;
 
 /**
  * Raumberechnungen
+ * 
  * @author Matthias Stickler
  * @version 1.0
  */
@@ -10,44 +11,62 @@ public class Raum {
 	private double b;
 	private double h;
 	private double r;
+
 	/**
-	 * Standardkonstruktor
+	 * Konstruktor für die Pyramide/Quader
+	 * 
+	 * @param a
+	 *            Seite a
+	 * @param h
+	 *            Höhe h
 	 */
-	public Raum(){
-		new Raum();
+	public Raum(double a, double b, double h) {
+		this.a = a;
+		this.b = b;
+		this.h = h;
+		this.r = 0;
 	}
+
 	/**
-	 * Konstruktor um die benötigten Variablen mitzugeben
-	 * @param seitea - Seite A
-	 * @param seiteb - Seite B
-	 * @param hoehe - die Hoehe
-	 * @param radius - der Radius
-	 */	
-	public Raum(double seitea, double seiteb, double hoehe, double radius){
-		this.a=seitea;
-		this.b=seiteb;
-		this.h=hoehe;
-		this.r=radius;
+	 * Konstruktor für den Kegel
+	 * 
+	 * @param r
+	 *            Radius r
+	 * @param h
+	 *            Höhe h
+	 */
+	public Raum(double r, double h) {
+		this.a = 0;
+		this.b = 0;
+		this.h = h;
+		this.r = r;
+
 	}
+
 	/**
 	 * Volumen der Pyramide berechnen
+	 * 
 	 * @return Das Volumen
 	 */
 	public double pyramide() {
-		return ((a*a)*h)/3;
+		return ((a * b) * h) / 3;
 	}
+
 	/**
 	 * Volumen des Kegels berechnen
+	 * 
 	 * @return Das Volumen
 	 */
 	public double kegel() {
-		return (Math.PI*(r*r))/3;
+		return (Math.PI * h * (r * r)) / 3;
 	}
+
 	/**
 	 * Volumen des Quaders berechnen
+	 * 
 	 * @return das Volumen
 	 */
 	public double quader() {
-		return a*b*h;
+		return a * b * h;
 	}
 }
