68a094ead7099b00f29aca6cd6dd1201643c33dc
omasrie <omasrie@student.tgm.ac.at>
2014-12-05 12:11:20 +0100

    Bugfix

--- teamarbeit/Flaeche.java
+++ teamarbeit/Flaeche.java
@@ -8,49 +8,66 @@ package teamarbeit;
  */
 
 public class Flaeche {
-	private double a, b, c, r; //Attribute für die Flächenberechnung
+	private double a, b, c, r; // Attribute für die Flächenberechnung
+
 	/**
 	 * Konstruktor für Rechtecke
-	 * @param a - Seite a
-	 * @param b - Seite b
+	 * 
+	 * @param a
+	 *            - Seite a
+	 * @param b
+	 *            - Seite b
 	 */
-	public flaeche(double a, double b) {
-		this.a = a; 
+	public Flaeche(double a, double b) {
+		this.a = a;
 		this.b = b;
 	}
+
 	/**
 	 * Konstruktor für die Rechtecksflächenberechnung
-	 * @param a Seite a
-	 * @param b Seite b
-	 * @param c Seite c
+	 * 
+	 * @param a
+	 *            Seite a
+	 * @param b
+	 *            Seite b
+	 * @param c
+	 *            Seite c
 	 */
-	public flaeche(double a, double b, doublce c) {
+	public Flaeche(double a, double b, double c) {
 		this.a = a;
 		this.b = b;
 		this.c = c;
 	}
+
 	/**
 	 * Konstruktor für die Dreiecksflächenberechnung
-	 * @param r - Radius
+	 * 
+	 * @param r
+	 *            - Radius
 	 */
-	public flaeche(double r){
+	public Flaeche(double r) {
 		this.r = r;
+	}
+
 	/**
 	 * Methode zum Berechnen der Rechtecksfläche
 	 */
 	public double rechteckA() {
-		return a*b;
+		return a * b;
 	}
+
 	/**
 	 * Methode zum Berechnen der Kreisfläche
 	 */
-	public kreisA(){
-		return Math.PI*r*r;
+	public double kreisA() {
+		return Math.PI * r * r;
 	}
+
 	/**
 	 * Methode zum Berechnen der Dreieicksfläche
 	 */
-	public dreieckA(){
-		double s = (a+b+c)/2;
-		return Math.sqrt(s*(s-a)*(s-b)*(s-c));
+	public double dreieckA() {
+		double s = (a + b + c) / 2;
+		return Math.sqrt(s * (s - a) * (s - b) * (s - c));
 	}
+}
